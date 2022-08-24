f76aa3b73678e0649e7d0e8657d7e5f6ae50f50a
Mahamadou Kabba <mkabba34@gmail.com>
2022-08-24 10:54:12 -0400

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+// delete src folder then recreate it and create index.js
+import React from "react";
+import ReactDOM from "react";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+// import express from 'express'  is same as const express = reuire('express')
+// to enable import syntax go to pacakge.json to add "type":"module" under "main": "index.js" change test to start
